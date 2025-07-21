24e0a8007980f21a865828dcbb64e86f33fde5b7
Nusrat Ahmmed Ekra <141204200+Nusratekra@users.noreply.github.com>
2025-07-21 23:59:23 +0600

    index.js

--- js filter()/index.js
+++ js filter()/index.js
@@ -3,3 +3,11 @@ var newn= n.filter(function (x){
     return x>10;
 })
 document.writeln(n);// .filter() this function give a new array based on condition
+
+// Arrow function
+const add = (a, b) => {
+  return a + b;
+}
+
+// যদি শুধু একটা এক্সপ্রেশন থাকে, তাহলে {}
+const add = (a, b) => a + b;
