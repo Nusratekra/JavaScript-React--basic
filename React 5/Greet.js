db700f4e057d8d55768b331196216c09cea178d7
Nusrat Ahmmed Ekra <141204200+Nusratekra@users.noreply.github.com>
2025-07-29 22:37:15 +0600

    Add files via upload

---
+++ React 5/Greet.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+//use arrowafunction
+/* function Greet(){
+    return <h1>"Beauty meant that you were good.<br></br> And being good meant being happy. <br></br> Happiness can be defined all kinds of ways, <br></br> but human beings, consciously or unconsciously,<br></br>  are always pulling for their own version of happiness. <br></br> Even people who want to die see death as a kind of solace, <br></br> and view ending their lives as the only way to make it there. <br></br> Happiness is the base unit of consciousness,<br></br>  our single greatest motivator."
+<br></br> -Mieko Kawakami(Breast & Eggs)</h1>
+} */
+//named export
+export const Greet = () =><h1>"Beauty meant that you were good.
+    <br></br> And being good meant being happy. 
+    <br></br> Happiness can be defined all kinds of ways, 
+    <br></br> but human beings, consciously or unconsciously,
+    <br></br>  are always pulling for their own version of happiness.
+     <br></br> Even people who want to die see death as a kind of solace, 
+     <br></br> and view ending their lives as the only way to make it there. 
+     <br></br> Happiness is the base unit of consciousness,<br></br>  our single greatest motivator."
+<br></br> -Mieko Kawakami(Breast & Eggs)</h1>
+
+//export default Greet;
